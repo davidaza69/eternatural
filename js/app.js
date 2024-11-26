703ec16a324869dced67b5fab38cf48d643d23fb
davidaza69 <davidaza000@gmail.com>
2024-11-25 23:26:29 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,37 @@
+const container = document.querySelector(".container_cap")
+
+if(!localStorage.todosCapitulos){
+    localStorage.setItem( "todosCapitulos", JSON.stringify( capitulos ) )
+}
+let misCapitulos = JSON.parse( localStorage.getItem( "todosCapitulos" ) )
+
+
+misCapitulos.forEach( function (cap){
+
+    const card = document.createElement("button")
+    card.classList.add("card")
+    card.style.backgroundImage = `url(${cap.portada})`
+    card.style.backgroundSize= "cover"
+    card.style.backgroundPosition= "center"
+
+    if(cap.habilitado){
+        card.classList.remove("disabled")
+    }else{
+        card.classList.add("disabled")
+        card.disabled = true
+    }
+
+    card.addEventListener( "click", function () {
+        console.log( cap )
+        localStorage.setItem("currentCapitulo", JSON.stringify(cap) )
+        localStorage.setItem("todosCapitulos", JSON.stringify(misCapitulos) )
+        window.location.href = cap.webPage
+    } )
+
+    card.innerHTML = `
+            <h1 class="numero">${cap.cap}</h1>
+    `
+
+    container.appendChild(card)
+
+} )
---
+++ js/capitulosbd.js
@@ -0,0 +1,31 @@
+// array de objetos
+let capitulos = [
+    {
+        cap: 1,
+        habilitado: true,
+        currentScene: 1,
+        portada: "../assets/img/intro.png",
+        webPage:"../pages/cap_1.html"
+    },
+    {
+        cap: 2,
+        habilitado: false,
+        currentScene: 1,
+        portada: "../assets/img/escarabajo.png",
+        webPage:"../pages/cap_2.html"
+    },
+    {
+        cap: 3,
+        habilitado: false,
+        currentScene: 1,
+        portada: "../assets/img/ave.png",
+        webPage:"../pages/cap_3.html"
+    },
+    {
+        cap: 4,
+        habilitado: false,
+        currentScene: 1,
+        portada: "../assets/img/boa.png",
+        webPage:"../pages/cap_4.html"
+    }
+]
